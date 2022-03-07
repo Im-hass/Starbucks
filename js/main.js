4317fdb3bc16d6037ce55377f6ad86ffb48b440e
Imhuiseon <huiseon4107@gmail.com>
2022-03-07 21:29:43 +0900

    Docs:  floatingObject 함수 설명 주석 추가

--- js/main.js
+++ js/main.js
@@ -113,6 +113,7 @@ function floatingObject(selector, delay, size) {
   );
 }
 
+// floatingObject(selector, delay, size);
 floatingObject('.floating1', 1, 15);
 floatingObject('.floating2', .5, 15);
 floatingObject('.floating3', 1.5, 20);
